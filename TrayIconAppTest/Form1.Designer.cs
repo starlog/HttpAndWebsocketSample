17b95144190be345052cf00dc75924590760d191
Felix Cho <felix.cho.kr@gmail.com>
2024-10-11 15:35:50 +0900

    Add project files.

---
+++ TrayIconAppTest/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TrayIconAppTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "전송 테스트 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(627, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "전송 테스트 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(446, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "sending {\"data\":\"12345678\"}";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 114);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(152, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 140);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "로컬 http, WebSocket 서버 기능 테스트 (felix@kcttel.com)";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ TrayIconAppTest/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace TrayIconAppTest
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+        private HttpListener httpListener;
+        private bool running = true;
+        private List<WebSocket> activeWebSockets = new List<WebSocket>();
+        private static Form1 _instance;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _instance = this;
+
+            // Create a context menu for the tray icon
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Show", null, OnShow);
+            trayMenu.Items.Add("Exit", null, OnExit);
+
+            // Create the tray icon and attach the menu
+            trayIcon = new NotifyIcon
+            {
+                Text = "WebSocket and HTTP Server",
+                Icon = new Icon(SystemIcons.Application, 40, 40),
+                ContextMenuStrip = trayMenu,
+                Visible = true
+            };
+            this.WindowState = FormWindowState.Minimized;
+
+            toolStripStatusLabel1.Text = "WebSocket and HTTP Server";
+            // Start the HTTP/WebSocket server in a background task
+            Task.Run(() => StartHttpServer());
+        }
+        private void OnShow(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            running = false;
+            httpListener?.Stop();
+            trayIcon.Visible = false; // Hide the tray icon when the application exits
+            Application.Exit();
+        }
+
+        private async Task StartHttpServer()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+            httpListener.Prefixes.Add("http://localhost:8080/ws/");
+            httpListener.Start();
+            Console.WriteLine("Server started at http://localhost:8080/");
+
+            while (running)
+            {
+                HttpListenerContext context = await httpListener.GetContextAsync();
+
+                if (context.Request.IsWebSocketRequest && context.Request.RawUrl == "/ws/")
+                {
+                    _ = HandleWebSocketRequest(context); // Handle WebSocket connections
+                }
+                else
+                {
+                    await HandleHttpRequest(context); // Handle HTTP requests
+                }
+            }
+        }
+
+        private async Task HandleHttpRequest(HttpListenerContext context)
+        {
+            // Get the 'data' query string parameter from the URL
+            string dataParam = context.Request.QueryString["data"];
+            if (!string.IsNullOrEmpty(dataParam))
+            {
+                Console.WriteLine($"Received HTTP data: {dataParam}");
+                await SendMessageToWebSocketClients(dataParam); // Send data to WebSocket clients
+            }
+            else
+            {
+                Console.WriteLine("No data parameter received.");
+            }
+
+            // Respond to the HTTP request
+            string responseMessage = !string.IsNullOrEmpty(dataParam)
+                ? $"<html><body><h1>Received data: {dataParam}</h1></body></html>"
+                : "<html><body><h1>No data received</h1></body></html>";
+
+            byte[] buffer = Encoding.UTF8.GetBytes(responseMessage);
+            context.Response.ContentLength64 = buffer.Length;
+            context.Response.ContentType = "text/html";
+
+            await context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            context.Response.Close();
+        }
+
+        private async Task HandleWebSocketRequest(HttpListenerContext context)
+        {
+            Console.WriteLine("WebSocket Request...");
+
+            WebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+            WebSocket webSocket = wsContext.WebSocket;
+
+            // Add the new WebSocket to the active list
+            lock (activeWebSockets)
+            {
+                activeWebSockets.Add(webSocket);
+            }
+
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Goodbye", CancellationToken.None);
+                        Console.WriteLine("WebSocket closed.");
+
+                        // Remove the WebSocket from the active list
+                        lock (activeWebSockets)
+                        {
+                            activeWebSockets.Remove(webSocket);
+                        }
+                    }
+                    else
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Console.WriteLine($"Received WebSocket message: {message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"WebSocket error: {ex.Message}");
+            }
+        }
+
+        private async Task SendMessageToWebSocketClients(string message)
+        {
+            var jsonObject = new { data = message };
+            string jsonMessage = JsonConvert.SerializeObject(jsonObject);
+            byte[] buffer = Encoding.UTF8.GetBytes(jsonMessage);
+
+            List<Task> sendTasks = new List<Task>();
+            lock (activeWebSockets)
+            {
+                foreach (var socket in activeWebSockets)
+                {
+                    if (socket.State == WebSocketState.Open)
+                    {
+                        sendTasks.Add(socket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None));
+                    }
+                }
+            }
+
+            await Task.WhenAll(sendTasks);
+            Console.WriteLine("Message sent to all connected WebSocket clients.");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await SendMessageToWebSocketClients(textBox1.Text);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await SendMessageToWebSocketClients("12345678");
+        }
+    }
+}
+ 
